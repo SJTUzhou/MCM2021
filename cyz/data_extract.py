f17517b1fdebcfa654c4bd16d374be86ad17ba1d
CHEN Yizhou <yizhou.chen@student-cs.fr>
2021-01-29 09:41:47 +0100

    direction factor

--- cyz/data_extract.py
+++ cyz/data_extract.py
@@ -16,11 +16,18 @@ info_list = []
 for direction in direction_list:
     temp_dataframe = df1.loc[df1["DIRECTION"]==direction]
     freight_rates = temp_dataframe["AMT"]
-    container_volumes = [int(element[:2]) for element in temp_dataframe["CNTR_TYPE"]]
+    container_volumes = pd.Series([int(element[:2]) for element in temp_dataframe["CNTR_TYPE"]])
     revenue = int(freight_rates.sum())
-    volume = np.sum(container_volumes)
+    volume = container_volumes.sum()
+    volume_counts = container_volumes.value_counts()
+    num_20 = 0
+    num_40 = 0
+    if 20 in volume_counts.keys():
+        num_20 = volume_counts[20]
+    if 40 in volume_counts.keys():
+        num_40 = volume_counts[40]
     freight_rate = freight_rates.mean()
-    myDict = {"DIRECTION": direction, "REVENUE":revenue, "SALES_VOLUME":volume, "FREIGHT_RATE":freight_rate}
+    myDict = {"DIRECTION": direction, "REVENUE":revenue, "SALES_VOLUME":volume, "FREIGHT_RATE":freight_rate, "NUM_40":num_40, "NUM_20":num_20}
     info_list.append(myDict) 
 
 info_df = pd.DataFrame(info_list)
