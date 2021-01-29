b957f7c9bcdd85131770bb526f11189818c897b5
triplrqin <qinzijian1999@qq.com>
2021-01-29 22:51:52 +0800

    update php input and topsis.csv

--- zjy/TOPSIS.py
+++ zjy/TOPSIS.py
@@ -20,7 +20,7 @@ arv_max=data.max(axis=0)
 for i in range(7):
     data[:,i]=(data[:,i]-arv_min[i])/(arv_max[i]-arv_min[i])
 
-w=[0,0.3045,0.5454,0,0,0.0486,0.1015]#权重向量
+w=[0,0.5454,0.3045,0,0,0.0486,0.1015]#权重向量
 
 score = np.zeros((data.shape[0], 1))
 score[:,0]=data[:,0]*w[0]+data[:,1]*w[1]+data[:,2]*w[2]+data[:,3]*w[3]+data[:,4]*w[4]+data[:,5]*w[5]+data[:,6]*w[6]
@@ -29,5 +29,5 @@ score[:,0]=data[:,0]*w[0]+data[:,1]*w[1]+data[:,2]*w[2]+data[:,3]*w[3]+data[:,4]
 df1 = pd.read_csv(path,sep=',')
 
 df1['score']=pd.DataFrame(score)
-
-df1.to_csv("TOPSIS.csv", index=False)
+df2 = df1.sort_values(by='score', ascending=False)
+df2.to_csv("TOPSIS.csv", index=False)
