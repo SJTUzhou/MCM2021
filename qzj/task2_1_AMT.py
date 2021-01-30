c7c6683c9603d6b7289c409311559b41cf2870a5
triplrqin <qinzijian1999@qq.com>
2021-01-30 19:17:50 +0800

    update_AMT_figure

--- qzj/task2_1_AMT.py
+++ qzj/task2_1_AMT.py
@@ -22,8 +22,49 @@ for seletion in top10_seletion:
         same_svvd = df1.loc[df1[4] == svvd]
         #ave_AMT = same_svvd[5].astype(float).mean()
         num = len(same_svvd)
+        same_time_svvd = pd.to_datetime(same_svvd[1])
+        time_list = pd.unique(same_time_svvd)
+        time_counts = same_time_svvd.value_counts(sort=False)
         
-            
+        time_list_str = pd.unique(same_svvd[1])
+        AMT_list = []
+        for t in time_list_str:
+            AMT_df = same_svvd.loc[same_svvd[1] == t]
+            AMT_list.append(AMT_df[5].astype(float).mean())
+            #print(2)
+
+        time_counts = pd.Series(AMT_list).value_counts(sort=False).reset_index()
+        # 提前了几天
+        time_before = time_list[-1] - time_list
+        time_before_days = time_before.astype('timedelta64[D]').astype(int)
+        
+        svvd_info_large = pd.concat([pd.DataFrame(time_counts['index']),pd.DataFrame(time_before_days)], axis=1)
+        svvd_info_large.rename({'index':'date', 1: 'count', 0:'before'}, axis=1, inplace=True)
+        before_list = pd.unique(svvd_info_large['before'])
+        sum_s_list=[]
+        for i in before_list:
+            s = svvd_info_large.loc[svvd_info_large['before'] == i]
+            sum_s = s['date'].sum()
+            sum_s_list.append(sum_s)
+            #tmp = pd.DataFrame({'count': [sum_s], 'before_days': [i])
+        svvd_info_large = pd.DataFrame({'count': sum_s_list, 'before_days': before_list})
+        #svvd_info_large.set_index('before')
+        #svvd_info_large.rename_axis(svvd, axis=1, inplace=True)
+        svvd_info_large['SVVD'] = svvd
+        #svvd_info_large.set_index('before_days')
+        tmp_large = pd.concat([tmp_large, svvd_info_large])
+        large_count = tmp_large.groupby(by='before_days')['count'].sum()/svvd_num
+        df2 = pd.DataFrame({'before_days':large_count.index, 'average_count': large_count.values})
+        df2['num'] = 'large'
+        #filename = 'qzj/habbits/' + seletion+'_large.csv'
+        figurename = 'qzj/mean_AMT/' + seletion+'.png'
+        ax = df2.plot(x='before_days',y='average_count',color='DarkBlue',xlim=[0,14])
+        ax.set_ylabel('Average Freight Rate')
+        ax.set_xlabel('Days Before Sailing')
+        fig = ax.get_figure()
+        fig.savefig(figurename)
+        
+        '''
         if num > 100:
             same_time_svvd = pd.to_datetime(same_svvd[1])
             time_list = pd.unique(same_time_svvd)
@@ -106,6 +147,7 @@ for seletion in top10_seletion:
             fig = ax.get_figure()
             fig.savefig(figurename)
             #df2.to_csv(filename)
+        '''
 
         df2['SVVD'] = svvd
         tmp_1 = pd.concat([tmp_1, df2])
